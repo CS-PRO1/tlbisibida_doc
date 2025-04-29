92451ffa78aaa4808635a48f91cfdfb5fea5bafb
Uravity-PRO <csprocsgo@gmail.com>
2025-04-29 11:51:05 +0300

    newicon

---
+++ lib/components/tusk_icons'.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/widgets.dart';
+
+class TuskIcons {
+  TuskIcons._();
+
+  static const _kFontFam = 'TuskIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData lab_icon = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
