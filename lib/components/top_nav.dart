97386cdeb3459de0d0a906e3f0c8a4979ccde24b
Uravity-PRO <csprocsgo@gmail.com>
2025-04-30 15:43:05 +0300

    side menu

--- lib/components/top_nav.dart
+++ lib/components/top_nav.dart
@@ -1,4 +1,7 @@
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tlbisibida_doc/components/tusk_icons.dart';
 import 'package:tlbisibida_doc/constants/constants.dart';
 import 'package:tlbisibida_doc/services/navigation/locator.dart';
 import 'package:tlbisibida_doc/services/navigation/navigation_service.dart';
@@ -27,9 +30,12 @@ class TopNavigationBar extends StatelessWidget implements PreferredSizeWidget {
                       color: cyan500,
                     )
                   : SizedBox(),
-              title: Text(title),
+              title: Text(
+                title,
+                style: TextStyle(color: cyan_navbar_600),
+              ),
               centerTitle: true,
-              iconTheme: const IconThemeData(color: cyan500),
+              iconTheme: const IconThemeData(color: cyan_navbar_600),
               elevation: 0,
               backgroundColor: cyan100,
               actions: [
@@ -48,7 +54,101 @@ class TopNavigationBar extends StatelessWidget implements PreferredSizeWidget {
                 // ),
                 IconButton(
                   onPressed: () {},
-                  icon: const Icon(Icons.notifications_none_rounded),
+                  icon: const Icon(
+                    Icons.notifications_none_rounded,
+                  ),
+                ),
+                // IconButton(
+                //     onPressed: () {},
+                //     icon: const Icon(
+                //       Icons.menu_rounded,
+                //     ))
+                SizedBox(
+                  width: 50,
+                  child: DropdownSearch<(IconData, String)>(
+                    mode: Mode.custom,
+                    // selectedItem: (Icons.menu, ''),
+                    compareFn: (item1, item2) => item1.$1 == item2.$2,
+                    items: (f, cs) => [
+                      (CupertinoIcons.chart_pie, 'الاحصائيات'),
+                      (TuskIcons.secretary, 'السكرتاريا'),
+                      (Icons.person, 'الملف الشخصي'),
+                      (Icons.settings, 'الإعدادات'),
+                      (Icons.info_outline_rounded, 'حول'),
+                      (Icons.logout_rounded, 'تسجيل الخروج'),
+                    ],
+                    // decoratorProps: DropDownDecoratorProps(
+                    //   decoration: InputDecoration(
+                    //     contentPadding: EdgeInsets.symmetric(vertical: 6),
+                    //     //filled: true,
+                    //     //fillColor: Color(0xFF1eb98f),
+                    //     border: OutlineInputBorder(
+                    //       borderSide: BorderSide(color: Colors.transparent),
+                    //       borderRadius: BorderRadius.circular(8),
+                    //     ),
+                    //     focusedBorder: OutlineInputBorder(
+                    //       borderSide: BorderSide(color: Colors.transparent),
+                    //       borderRadius: BorderRadius.circular(8),
+                    //     ),
+                    //     enabledBorder: OutlineInputBorder(
+                    //       borderSide: BorderSide(color: Colors.transparent),
+                    //       borderRadius: BorderRadius.circular(8),
+                    //     ),
+                    //   ),
+                    // ),
+                    dropdownBuilder: (ctx, selectedItem) => Icon(
+                      Icons.menu,
+                      color: cyan_navbar_600,
+                    ),
+                    popupProps: PopupProps.menu(
+                      itemBuilder: (context, item, isDisabled, isSelected) {
+                        return ListTile(
+                          contentPadding:
+                              EdgeInsets.symmetric(vertical: 0, horizontal: 12),
+                          leading: Icon(item.$1, color: Colors.white),
+                          title: Text(
+                            item.$2,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        );
+                      },
+                      fit: FlexFit.loose,
+                      menuProps: MenuProps(
+                        backgroundColor: Colors.transparent,
+                        elevation: 0,
+                        //margin: EdgeInsets.only(top: 16),
+                      ),
+                      containerBuilder: (ctx, popupWidget) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: 12),
+                              child: Image.asset(
+                                'assets/icons/arrow-up.png',
+                                color: cyan400,
+                                height: 14,
+                              ),
+                            ),
+                            Flexible(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: cyan400,
+                                  shape: BoxShape.rectangle,
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: popupWidget,
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
                 ),
               ],
             );
--- lib/components/tusk_icons.dart
+++ lib/components/tusk_icons.dart
@@ -6,8 +6,10 @@ class TuskIcons {
   static const _kFontFam = 'TuskIcons';
   static const String? _kFontPkg = null;
 
-  static const IconData lab_icon =
+  static const IconData secretary=
       IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData lab_icon =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData teeth_open =
       IconData(0xf62f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
