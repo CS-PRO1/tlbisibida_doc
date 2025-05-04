aa6c3876ef152e3987bd373ec8c7c9bf13e20287
Uravity-PRO <csprocsgo@gmail.com>
2025-05-04 13:23:49 +0300

    icon

--- lib/components/tusk_icons.dart
+++ lib/components/tusk_icons.dart
@@ -6,10 +6,8 @@ class TuskIcons {
   static const _kFontFam = 'TuskIcons';
   static const String? _kFontPkg = null;
 
-  static const IconData secretary=
-      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData lab_icon =
-      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData teeth_open =
-      IconData(0xf62f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData secretary = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData lab_icon = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dent_tools = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData teeth_open = IconData(0xf62f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
